c101f0a711f94c75d335ea7121a0d6f29bdc0c92
ch1ple <teavis@mail.ru>
2024-12-02 20:18:21 +0300

    initial commit

---
+++ src/main/java/me/ch1ple/pro/sky/exam/question7/Application.java
@@ -0,0 +1,13 @@
+package me.ch1ple.pro.sky.exam.question7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/ch1ple/pro/sky/exam/question7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ch1ple.pro.sky.exam.question7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
